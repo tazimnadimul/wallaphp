3d4b88c5d92dfcdf9c36b7fac63b39fa9ac8920f
Manel <manumcraft@gmail.com>
2020-12-04 13:07:21 +0100

    header and footer stylished

--- header.php
+++ header.php
@@ -3,7 +3,23 @@
 <head>
 <title>Page Title</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link href="css/sticky-footer.css" rel="stylesheet">
 </head>
 <body>
-
+<nav class="navbar navbar-dark bg-info">
+  <a class="navbar-brand" href="#">
+    <img src="img/brand.png" width="70" height="50" class="d-inline-block align-top" alt="" loading="lazy">
+    <b>Wallaphp</b>
+	<span style="display: block;/*! margin-left: 80px; */position: absolute;left: 110px;top: 50px;font-size: 16px;">Tu mercadillo de estar por casa.</span>
+  </a>
+<ul style="min-height:70px;" class="nav nav-pills nav-fill nav-justified">
+  <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button onclick="location.href = 'register.php';" type="button" class="btn btn-secondary btn-sm"><b>Register</b></button>
+  <button onclick="location.href = 'login.php';" type="button" class="btn btn-secondary"><b>Login</b></button>
+</div>
+</div>
+</nav>
